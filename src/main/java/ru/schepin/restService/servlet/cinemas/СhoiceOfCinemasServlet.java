fe79b9cfe13b97eb2470583bdf214d09fa44784e
Schepin Dmitry <bright1308@gmail.com>
2018-10-22 14:40:22 +0700

    Add servlets for choice cinemas and halls. Add contextListener

---
+++ src/main/java/ru/schepin/restService/servlet/cinemas/СhoiceOfCinemasServlet.java
@@ -0,0 +1,19 @@
+package ru.schepin.restService.servlet.cinemas;
+
+import ru.schepin.restService.util.Urls;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class СhoiceOfCinemasServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        System.out.println("doGet is work!(Рендеринг страницы)");
+        req.getRequestDispatcher(Urls.index).forward(req, resp);
+    }
+}
